bbaf184a5f988f067c8646a46edf5d2101d56e7c
LAURENT Louis <louis_laurent@aol.fr>
2020-04-11 08:47:59 +0200

    add get function for secret index of the key
    allow to check if the key if well form and get index of the key for decrypt a message
    add coveralls to github action

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub mod binary_matrix_encryption {
         pub fn split_number(&mut self, character: u8) {
             self.heavyweight_bits = character & 0b11110000;
             self.lightweight_bits = character & 0b00001111;
-        } 
+        }
     }
 
     pub fn read_file(filename: String) -> String {
@@ -70,6 +70,79 @@ pub mod binary_matrix_encryption {
             .fold(0, |acc, (i, _x)| acc + compute_binary_value(i  as u8));
 
         return Ok(number_convertion);
+    }
+
+    pub fn get_secret_key_index(key_matrix: &[String; 4]) -> Result<[i8; 4], &'static str> {
+        if ! key_is_well_form(key_matrix) {
+            return Err("The matrix key doesn't conform to the specifications");
+        }
+
+        let mut secret_lines: [i8; 4] = [-1; 4];
+
+        for i in 0..7 {
+            let current_bits: [char; 4] = [ 
+                key_matrix[0].as_bytes()[i] as char,
+                key_matrix[1].as_bytes()[i] as char,
+                key_matrix[2].as_bytes()[i] as char,
+                key_matrix[3].as_bytes()[i] as char
+            ];
+
+            let index_detected_secret_line = match check_secret_lines(current_bits) {
+                Some(index_value) => index_value as i8,
+                None => -1,
+            };
+            
+            if index_detected_secret_line == -1 {
+                continue;
+            }
+
+            if secret_lines[index_detected_secret_line as usize] != -1 {
+                return Err("The key desn't have a clear sequence of identity lines");
+            }
+
+            secret_lines[index_detected_secret_line as usize] = i as i8;
+        }
+
+        return Ok(secret_lines);
+    }
+
+    pub fn key_is_well_form(key_matrix: &[String; 4]) -> bool {
+        if key_matrix.len() != 4 {
+            return false;
+        }
+
+        let good_number_bits = key_matrix.iter()
+            .filter(|matrix_line| matrix_line.len() != 8)
+            .fold(false, |acc, _x| acc == true);
+        
+        if ! good_number_bits {
+            return false;
+        }
+
+        return true;
+    }
+
+    pub fn check_secret_lines(current_bits: [char; 4]) -> Option<u8> {
+        let first_line_secret_pattern = ['1', '0', '0', '0'];
+        let second_line_secret_pattern = ['0', '1', '0', '0'];
+        let third_line_secret_pattern = ['0', '0', '1', '0'];
+        let fourth_line_secret_pattern = ['0', '0', '0', '1'];
+
+        if current_bits == first_line_secret_pattern {
+            return Some(0);
+
+        }else if current_bits == second_line_secret_pattern {
+            return Some(1);
+
+        }else if current_bits == third_line_secret_pattern {
+            return Some(2);
+
+        }else if current_bits == fourth_line_secret_pattern {
+            return Some(3);
+        }
+
+        return None;
+
     }
 
     // let splited_as_8bits_string: Vec<String> = binary_string
